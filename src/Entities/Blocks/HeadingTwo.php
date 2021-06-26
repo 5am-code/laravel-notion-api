f4ca7a4e1f165343e36fa1f78ed9274b858e0715
JohGuentner <3506359+johguentner@users.noreply.github.com>
2021-06-26 23:13:48 +0200

    implement notion block types as classes
    - add easy content and text access for rich-text- (all supported blocks, exept page-blocks) and child-page-blocks
    - add fromResponse- and mapTypeToClass-method to Block::class (similar to the implementations within Property::class)

---
+++ src/Entities/Blocks/HeadingTwo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FiveamCode\LaravelNotionApi\Entities\Blocks;
+
+use DateTime;
+use Illuminate\Support\Arr;
+use FiveamCode\LaravelNotionApi\Entities\Entity;
+use FiveamCode\LaravelNotionApi\Exceptions\HandlingException;
+
+/**
+ * Class HeadingTwo
+ * @package FiveamCode\LaravelNotionApi\Entities\Blocks
+ */
+class HeadingTwo extends TextBlock
+{
+}
--- src/Entities/Collections/BlockCollection.php
+++ src/Entities/Collections/BlockCollection.php
@@ -12,11 +12,40 @@ use FiveamCode\LaravelNotionApi\Entities\Blocks\Block;
  */
 class BlockCollection extends EntityCollection
 {
+    private bool $showUnsupported = false;
+
+    public function withUnsupported(): BlockCollection
+    {
+        $this->showUnsupported = true;
+        return $this;
+    }
+
     protected function collectChildren(): void
     {
         $this->collection = new Collection();
-        foreach ($this->rawResults as $blockChild) {
-            $this->collection->add(new Block($blockChild));
+        foreach ($this->rawResults as $blockChildContent) {
+            $this->collection->add(Block::fromResponse($blockChildContent));
         }
+    }
+
+    public function asCollection(): Collection
+    {
+        $collection =parent::asCollection();
+        if ($this->showUnsupported) {
+            return $collection;
+        } else {
+            return $collection->filter(function($block){
+                return $block->getType() != 'unsupported';
+            });
+        }
+    }
+
+    public function asTextCollection(): Collection
+    {
+        $textCollection = new Collection();
+        foreach ($this->asCollection() as $block) {
+            $textCollection->add($block->asText());
+        }
+        return $textCollection;
     }
 }
