32fbfc6f8b822bfa0c9ff579c1d9c22ab5a2abd9
Di <scharf.diana@googlemail.com>
2022-07-09 16:21:28 +0200

    Apply fixes from StyleCI (#70)

--- tests/EndpointDatabaseTest.php
+++ tests/EndpointDatabaseTest.php
@@ -158,7 +158,7 @@ class EndpointDatabaseTest extends NotionApiTest
         Notion::database('8284f3ff77e24d4a939d19459e4d6bdc')->query();
     }
 
-    ## EDGE CASES
+    //# EDGE CASES
 
     /** @test */
     public function it_queries_a_database_with_a_rollup_property_with_empty_selects()
@@ -166,7 +166,7 @@ class EndpointDatabaseTest extends NotionApiTest
         // success /v1/databases/DATABASE_DOES_EXIST/query
         Http::fake([
             'https://api.notion.com/v1/databases/11971214ce574df7a58389c1deda61d7/query*' => Http::response(
-                json_decode(file_get_contents("tests/stubs/endpoints/databases/response_query_rollup_empty_select_200.json"), true),
+                json_decode(file_get_contents('tests/stubs/endpoints/databases/response_query_rollup_empty_select_200.json'), true),
                 200,
                 ['Headers']
             ),
