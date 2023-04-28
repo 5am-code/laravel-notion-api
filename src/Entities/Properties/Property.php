f19db046aa0360d69960433dfe3083bc7184e2e1
johguentner <johannes.guentner@it-guentner.de>
2023-04-28 23:47:50 +0900

    add property type names within `Property::class`

--- src/Entities/Properties/Property.php
+++ src/Entities/Properties/Property.php
@@ -11,6 +11,26 @@ use Illuminate\Support\Arr;
  */
 class Property extends Entity
 {
+    const TITLE = 'title';
+    const RICH_TEXT = 'rich_text';
+    const NUMBER = 'number';
+    const SELECT = 'select';
+    const MULTI_SELECT = 'multi_select';
+    const DATE = 'date';
+    const PEOPLE = 'people';
+    const FILES = 'files';
+    const CHECKBOX = 'checkbox';
+    const URL = 'url';
+    const EMAIL = 'email';
+    const PHONE_NUMBER = 'phone_number';
+    const FORMULA = 'formula';
+    const RELATION = 'relation';
+    const ROLLUP = 'rollup';
+    const CREATED_TIME = 'created_time';
+    const CREATED_BY = 'created_by';
+    const LAST_EDITED_TIME = 'last_edited_time';
+    const LAST_EDITED_BY = 'last_edited_by';
+
     /**
      * @var string
      */
