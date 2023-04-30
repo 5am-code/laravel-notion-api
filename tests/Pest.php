142d84c5f814c013499cf006d5da4c82be45603f
johguentner <johannes.guentner@it-guentner.de>
2023-04-30 11:13:44 +0900

    fix: ignore .env.testing file if it does not exist - .env.testing is only required during local tests, especially for creating new snapshots, on ci it is not required

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,8 +4,11 @@ use Dotenv\Dotenv;
 use FiveamCode\LaravelNotionApi\Tests\NotionApiTest;
 use Illuminate\Support\Facades\Config;
 
+// uses(NotionApiTest::class)->in(__DIR__);
 uses(NotionApiTest::class)->beforeEach(function () {
-    $dotenv = Dotenv::createImmutable(__DIR__.'/..', '.env.testing');
-    $dotenv->load();
-    Config::set('laravel-notion-api.notion-api-token', env('NOTION_API_TOKEN'));
+    if (file_exists(__DIR__ . '/../.env.testing')) {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/..', '.env.testing');
+        $dotenv->load();
+    }
+    Config::set('laravel-notion-api.notion-api-token', env('NOTION_API_TOKEN', ''));
 })->in(__DIR__);
