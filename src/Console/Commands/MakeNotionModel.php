8f80b4a1fdc974a0f3b6a2083f06eeb105fcc811
johguentner <johannes.guentner@it-guentner.de>
2023-06-16 15:20:03 +0200

    move make-notion-model content to blade - prototypical

--- src/Console/Commands/MakeNotionModel.php
+++ src/Console/Commands/MakeNotionModel.php
@@ -64,30 +64,7 @@ class MakeNotionModel extends Command
         }
 
         $contents = "<?php
-namespace App\NotionModels;
 
-use FiveamCode\LaravelNotionApi\Models\NotionModel;
-
-/** 
-{$phpDocsProperties}*/
-class {$databaseName} extends NotionModel
-{
-    public static \$databaseId = '{$databaseId}';
-
-    public static \$cacheDurationInSeconds = 0;
-
-    public static \$convertPropsToText = false;
-
-    public static \$visible = [
-$visibleArray    ];
-
-    public static \$propertyTypeMap = [
-$propertyTypeMape    ];
-    
-    public static \$propertyTitleMap = [
-$propertyTitleMap    ];
-
-}
 
 ";
         File::ensureDirectoryExists('app/NotionModels');
